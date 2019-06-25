e55bb87865e87be9b96434348a88cfa3c79a5068
Taaku18 <45324516+Taaku18@users.noreply.github.com>
2019-06-24 21:37:48 -0700

    Reformat

--- logger/logger.py
+++ logger/logger.py
@@ -76,20 +76,35 @@ class Logger(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self, payload):
-        if payload.guild_id != self.bot.guild.id:
+        if payload.guild_id != self.bot.guild_id:
             return
         channel = await self.get_log_channel()
         message = payload.cached_message
+
         if message:
-            await channel.send(embed=self.make_embed(f'A message sent by {message.author.display_name} ({message.author.name}#{message.author.discriminator} || {message.author.id}) has been deleted from {message.channel.mention}.',
-                                                     escape_mentions(message.content), fields=[('Message ID:', payload.message_id, True), ('Channel ID:', payload.channel_id, True), ('Message sent on:', message.created_at.strftime('%b %-d at %-I:%S %p %Z UTC'), True)]))
+            return await channel.send(embed=self.make_embed(
+                f'A message sent by {message.author.name}#{message.author.discriminator} '
+                f'({message.author.id}) has been deleted from #{message.channel.name}.',
+                escape_mentions(message.content),
+                fields=[('Message ID:', payload.message_id, True),
+                        ('Channel ID:', payload.channel_id, True),
+                        ('Message sent on:', message.created_at.strftime('%b %-d at %-I:%M %p'), True)
+                        ]
+            ))
+
+        payload_channel = self.bot.guild.get_channel(payload.channel_id)
+        if payload_channel is not None:
+            channel_text = payload_channel.name
         else:
-            payload_channel = self.bot.guild.get_channel(payload.channel_id)
-            if payload_channel is not None:
-                channel_text = payload_channel.mention
-            else:
-                channel_text = '#deleted-channel'
-            await channel.send(embed=self.make_embed(f'A message was deleted in {channel_text}.', 'The message content cannot be found, a further message may follow if this message was not deleted by the author.',  fields=[('Message ID:', payload.message_id, True), ('Channel ID:', payload.channel_id, True)]))
+            channel_text = 'deleted-channel'
+        return await channel.send(embed=self.make_embed(
+            f'A message was deleted in #{channel_text}.',
+            'The message content cannot be found, a further message may '
+            'follow if this message was not deleted by the author.',
+            fields=[('Message ID:', payload.message_id, True),
+                    ('Channel ID:', payload.channel_id, True)
+                    ]
+        ))
 
     @commands.Cog.listener()
     async def on_raw_bulk_message_delete(self, payload):
@@ -99,97 +114,118 @@ class Logger(commands.Cog):
 
         messages = sorted(payload.cached_messages, key=lambda msg: msg.created_at)
         message_ids = payload.message_ids
-        known_message_ids = set()
         upload_text = 'Here are the messages that were deleted:\n'
+
         if not messages:
             upload_text += 'There are no known messages.\n'
-            unknown_message_ids = message_ids
+            upload_text += 'Unknown message IDs: ' + ', '.join(map(str, message_ids)) + '.'
         else:
+            known_message_ids = set()
             for message in messages:
                 known_message_ids.add(message.id)
-                time = message.created_at.strftime('%b %-d at %-I:%S %p %Z UTC')
-                upload_text += f'{time} {message.author.display_name} ({message.author.name}#{message.author.discriminator} || {message.author.id}), message ID: {message.id}. Content: {message.content}\n'
+                time = message.created_at.strftime('%b %-d at %-I:%M %p')
+                upload_text += f'{time} {message.author.name}#{message.author.discriminator} ({message.author.id}). ' \
+                    f'Message ID: {message.id}. {message.content}\n'
             unknown_message_ids = message_ids ^ known_message_ids
-        if unknown_message_ids:
-            upload_text += 'Unknown message IDs: ' + ', '.join(map(str, unknown_message_ids)) + '.'
+            if unknown_message_ids:
+                upload_text += 'Unknown message IDs: ' + ', '.join(map(str, unknown_message_ids)) + '.'
 
         payload_channel = self.bot.guild.get_channel(payload.channel_id)
         if payload_channel is not None:
-            channel_text = payload_channel.mention
+            channel_text = payload_channel.name
         else:
-            channel_text = '#deleted-channel'
+            channel_text = 'deleted-channel'
 
         try:
-            async with self.bot.session.post(
-                    'https://hasteb.in/documents', data=upload_text
-            ) as resp:
+            async with self.bot.session.post('https://hasteb.in/documents', data=upload_text) as resp:
                 key = (await resp.json())["key"]
-                await channel.send(embed=self.make_embed(f'Multiple messages deleted from {channel_text}.', f'Deleted messages: https://hasteb.in/{key}.',
-                                                   fields=[('Channel ID:', payload.channel_id, True)]))
+                return await channel.send(embed=self.make_embed(
+                    f'Multiple messages deleted from #{channel_text}.',
+                    f'Deleted messages: https://hasteb.in/{key}.',
+                    fields=[('Channel ID:', payload.channel_id, True)]
+                ))
         except (JSONDecodeError, ClientResponseError, IndexError):
-            await channel.send(embed=self.make_embed(f'Multiple messages deleted from {channel_text}.', 'Failed to upload to Hastebin. Deleted message IDs: ' + ', '.join(map(str, message_ids)) + '.', fields=[('Channel ID', payload.channel_id, True)]))
+            return await channel.send(embed=self.make_embed(
+                f'Multiple messages deleted from {channel_text}.',
+                'Failed to upload to Hastebin. Deleted message IDs: ' + ', '.join(map(str, message_ids)) + '.',
+                fields=[('Channel ID', payload.channel_id, True)]
+            ))
 
     @commands.Cog.listener()
     async def on_raw_message_edit(self, payload):
-        channel_id = payload.data['channel_id']
-        message_id = payload.data['id']
+        channel_id = int(payload.data['channel_id'])
+        message_id = int(payload.data['id'])
+
+        new_content = payload.data.get('content')
+        if new_content is None:
+            # Currently does not support Embed edits
+            return
 
         payload_channel = self.bot.guild.get_channel(channel_id)
-
         if payload_channel is not None:
             if payload_channel.guild.id != self.bot.guild_id:
                 return
-            channel_text = payload_channel.mention
+            channel_text = payload_channel.name
         else:
-            channel_text = '#deleted-channel'
+            channel_text = 'deleted-channel'
 
         channel = await self.get_log_channel()
-
         message = payload.cached_message
-        new_content = payload.data.get('content')
 
         if message:
-            old_content = message.content
-            await channel.send(embed=self.make_embed(f'A message was updated in {channel_text}.',
-                                               fields=[('Before', old_content, False),
-                                                       ('After', new_content or 'No content change (possibly due to an embed edit).', False), ('Message ID:', message_id, True),
-                                                       ('Channel ID:', channel_id, True), ('Message sent on:', message.created_at.strftime('%b %-d at %-I:%S %p %Z UTC'), True)]))
+            return await channel.send(embed=self.make_embed(
+                f'A message was updated in #{channel_text}.',
+                fields=[('Before', message.content, False),
+                        ('After', new_content, False),
+                        ('Message ID:', message_id, True),
+                        ('Channel ID:', channel_id, True),
+                        ('Message sent on:', message.created_at.strftime('%b %-d at %-I:%M %p'), True)
+                        ]
+            ))
 
         else:
             if payload_channel is not None:
                 try:
                     message = await payload_channel.fetch_message(message_id)
-                    await channel.send(embed=self.make_embed(f'A message was updated in {channel_text}.',
-                                                   'The former message content cannot be found.',
-                                                   fields=[('Now',
-                                                            new_content or 'No content change (possibly due to an embed edit).',
-                                                            False), ('Message ID:', message_id, True),
-                                                           ('Channel ID:', channel_id, True), ('Message sent on:',
-                                                                                               message.created_at.strftime(
-                                                                                                   '%b %-d at %-I:%S %p %Z UTC'),
-                                                                                               True)]))
+                    return await channel.send(embed=self.make_embed(
+                        f'A message was updated in #{channel_text}.',
+                        'The former message content cannot be found.',
+                        fields=[('Now', new_content, False),
+                                ('Message ID:', message_id, True),
+                                ('Channel ID:', channel_id, True),
+                                ('Message sent on:', message.created_at.strftime('%b %-d at %-I:%M %p'), True)
+                                ]
+                    ))
                 except NotFound:
                     pass
-            await channel.send(embed=self.make_embed(f'A message was updated in {channel_text}.',
-                                               'The former message content cannot be found.',
-                                            fields=[('Now',
-                                                    new_content or 'No content change (possibly due to an embed edit).',
-                                                    False), ('Message ID:', message_id, True),
-                                                   ('Channel ID:', channel_id, True)]))
+            return await channel.send(embed=self.make_embed(
+                f'A message was updated in {channel_text}.',
+                'The former message content cannot be found.',
+                fields=[('Now', new_content, False),
+                        ('Message ID:', message_id, True),
+                        ('Channel ID:', channel_id, True)
+                        ]
+            ))
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if member.guild.id != self.bot.guild.id:
+        if member.guild.id != self.bot.guild_id:
             return
         channel = await self.get_log_channel()
-        await channel.send(embed=self.make_embed('Member Joined', f'{member.display_name} ({member.name}#{member.discriminator} || {member.id}) has joined.'))
+        await channel.send(embed=self.make_embed(
+            'Member Joined',
+            f'{member.name}#{member.discriminator} ({member.id}) has joined.'
+        ))
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        if member.guild.id != self.bot.guild.id:
+        if member.guild.id != self.bot.guild_id:
             return
         channel = await self.get_log_channel()
-        await channel.send(embed=self.make_embed('Member Joined', f'{member.display_name} ({member.name}#{member.discriminator} || {member.id}) has left.'))
+        await channel.send(embed=self.make_embed(
+            'Member Joined',
+            f'{member.name}#{member.discriminator} ({member.id}) has left.'
+        ))
 
     def make_embed(self, title, description='', *, time=None, fields=None):
         embed = Embed(title=title, description=description, color=self.bot.main_color)

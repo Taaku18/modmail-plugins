b752df19b2660b323f92302574dfc3b178444c2e
Taaku18 <45324516+Taaku18@users.noreply.github.com>
2019-11-26 03:57:29 -0800

    Add fox and shiba

--- animals/animals.py
+++ animals/animals.py
@@ -9,7 +9,7 @@ class Animals(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=["cat"])
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def meow(self, ctx):
         """
@@ -23,7 +23,7 @@ class Animals(commands.Cog):
         embed.set_image(url=cat)
         return await ctx.channel.send(embed=embed)
 
-    @commands.command()
+    @commands.command(aliases=["dog"])
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def woof(self, ctx):
         """
@@ -35,6 +35,34 @@ class Animals(commands.Cog):
             dog = (await r.json())["url"]
         embed = discord.Embed(title=":dog: ~woof~")
         embed.set_image(url=dog)
+        return await ctx.channel.send(embed=embed)
+
+    @commands.command(aliases=["fox"])
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def floof(self, ctx):
+        """
+        Random fox pic.
+
+        API from randomfox.ca!
+        """
+        async with self.bot.session.get("https://randomfox.ca/floof/") as r:
+            fox = (await r.json())["image"]
+        embed = discord.Embed(title=":fox: Here come's floofy")
+        embed.set_image(url=fox)
+        return await ctx.channel.send(embed=embed)
+
+    @commands.command(aliases=["shiba"])
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def shibe(self, ctx):
+        """
+        Random Shiba Inu pic.
+
+        API from shibe.online!
+        """
+        async with self.bot.session.get("http://shibe.online/api/shibes") as r:
+            shiba = (await r.json())[0]
+        embed = discord.Embed(title=":dog2: Peekaboo Shiba's here!")
+        embed.set_image(url=shiba)
         return await ctx.channel.send(embed=embed)
 
 
